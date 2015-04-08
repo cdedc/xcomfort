5fee582166c0d47164ea8caa8460b8e8d5f268e1
cdedc <cdedc@users.noreply.github.com>
2015-04-08 15:00:44 +0200

    Update xcomclient.c

--- xcomclient.c
+++ xcomclient.c
@@ -16,9 +16,9 @@ int main(int argc, char *argv[]) {
 		printf("Cant find / claim / open USB device.\n");
 		exit(EXIT_FAILURE); 		// FAILURE
 	}
-	// lxc_get_stats(dev);
-	lxc_assign_sensor(dev, 8);
-	while (1) lxc_poll_rx(dev);
+	// lxc_get_stats(dev);			// get some status from the USB gateway itself
+	lxc_assign_sensor(dev, 8);		// simulate a BASIC MODE assignment of a sensor to a datapoint
+	while (1) lxc_poll_rx(dev);		// then receive any messages that fly around and log them to the screen until ^c
 	lxc_close(dev);
 	lxc_exit();
 
